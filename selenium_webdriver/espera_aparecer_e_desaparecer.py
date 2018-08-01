a934fadad5e2d12255df452a7497057b030afc2d
Reinaldo Rossetti <reinaldo.rossetti@outlook.com>
2018-08-01 14:39:24 -0300

    Create espera_aparecer_e_desaparecer.py

---
+++ selenium_webdriver/espera_aparecer_e_desaparecer.py
@@ -0,0 +1,20 @@
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium import webdriver
+
+def find(driver):
+    return driver.find_element_by_css_selector('div[id="loading-component"][class*=show]')
+
+def wait_load(driver, timeout=30):
+    """Clear the cookies and cache for the ChromeDriver instance."""
+    # navigate to the settings page
+    driver.get('chrome://settings/clearBrowserData')
+
+    # wait to appear
+    wait = WebDriverWait(driver, 5)
+    wait.until(find(driver))
+    # wait to be gone
+    wait = WebDriverWait(driver, timeout)
+    wait.until_not(find(driver))
+    
+driver = webdriver.Chrome()
+wait_load(driver)
