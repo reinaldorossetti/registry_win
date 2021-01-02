46859d1e9caf27503e22adb4f77c849d0df26e05
reinaldorossetti <reinaldo.rossetti@outlook.com>
2021-01-02 00:15:38 -0300

    corrigindo mensagem

--- test2_move_cursor.py
+++ test2_move_cursor.py
@@ -1,7 +1,7 @@
 from selenium.webdriver import ActionChains
 from selenium import webdriver
 
-driver = webdriver.Chrome(executable_path="/home/dev/bin/chromedriverLinux64")
+driver = webdriver.Chrome()
 driver.maximize_window()
 
 driver.get("http://www.w3schools.com/js/tryit.asp?filename=tryjs_imagemap")
@@ -11,10 +11,14 @@ driver.implicitly_wait(15)
 test = driver.find_element_by_css_selector("#iframeResult")
 driver.switch_to.frame("iframeResult")
 
-Mercury = driver.find_element_by_css_selector("area[alt=\"Venus\"]")
+# driver.find_element_by_css_selector("area[shape=\"circle\"][alt=\"Venus\"]").click()
+# driver.execute_script("document.querySelector('area[href =\"venus.htm\"]').click()")
 
+element_mercury = driver.find_element_by_css_selector("area[shape=\"circle\"][alt=\"Venus\"]")
+
+print(element_mercury.rect)
 actions = ActionChains(driver)
-actions.move_to_element(Mercury)
+actions.move_to_element(element_mercury)
 actions.click().perform()
 
 
