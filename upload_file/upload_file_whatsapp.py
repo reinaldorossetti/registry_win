257332c8ed992584068df493f247a5513901b2f1
reiload <reiload@gmail.com>
2021-01-02 01:51:32 -0300

    melhorando a brincadeira de ano novo.

--- upload_file/upload_file_whatsapp.py
+++ upload_file/upload_file_whatsapp.py
@@ -1,23 +1,27 @@
+import random
 from selenium import webdriver
-from os import getcwd
+from os import getcwd, listdir, path
 
 driver = webdriver.Chrome()
 driver.get('https://web.whatsapp.com/')
 driver.implicitly_wait(30)
 driver.set_page_load_timeout(30)
 
-name = "Diogo"
-image_name = "ano_novo.gif"
-filepath = "{}\\arquivo\\{}".format(getcwd(), image_name)
+nameList = ["Diogo","ruby","Python"]
+dir = "{}\\arquivo\\".format(getcwd())
 
 input('Enter anything after scanning QR code')
 
-driver.find_element_by_css_selector('#pane-side span[title*="{}"]'.format(name)).click()
-driver.find_element_by_css_selector('div[role=button] span[data-icon="clip"]').click()
+for name in nameList:
+    files = [f for f in listdir(dir) if path.isfile(path.join(dir, f))]
+    filepath = "{}\\arquivo\\{}".format(getcwd(), random.choice(files))
 
-driver.find_element_by_xpath(
-    '//input[@accept="image/*,video/mp4,video/3gpp,video/quicktime"]').send_keys(filepath)
+    driver.find_element_by_css_selector('#pane-side span[title*="{}"]'.format(name)).click()
+    driver.find_element_by_css_selector('div[role=button] span[data-icon="clip"]').click()
 
-driver.find_element_by_css_selector("div[data-animate-media-caption='true'] div[class*='copyable-text selectable-text']").\
-    send_keys("Feliz Ano Novo!!! Sucesso...")
-driver.find_element_by_css_selector('span[data-testid="send"]').click()
+    driver.find_element_by_xpath(
+        '//input[@accept="image/*,video/mp4,video/3gpp,video/quicktime"]').send_keys(filepath)
+
+    driver.find_element_by_css_selector("div[data-animate-media-caption='true'] div[class*='copyable-text selectable-text']").\
+        send_keys("Feliz Ano Novo!!! Sucesso...")
+    driver.find_element_by_css_selector('span[data-testid="send"]').click()
